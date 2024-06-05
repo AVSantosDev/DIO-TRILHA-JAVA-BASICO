af83111eb954e48ec617cf5bb55b6773c9c7d639
Andre.vsantos <andrev0795@hotmail.com>
2024-06-04 22:51:00 -0300

    Sequencia aula

---
+++ Controle de fluxo/src/ClasseEletronico.java
@@ -0,0 +1,15 @@
+public class ClasseEletronico {
+
+    public static void main(String[] args){
+        
+        double saldo = 25.0;
+        double valorSolicitado = 40.0;
+
+        if (valorSolicitado < saldo)
+            saldo = saldo - valorSolicitado;
+
+        System.out.println(saldo);
+
+
+    }
+}
